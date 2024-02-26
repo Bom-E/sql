0907ad1bee70b41c37107d68ff8abe8eb955eb18
Bom-E <j32575086@gmail.com>
2024-02-26 17:52:40 +0900

    24.02.26

--- lib24.02.23.sql
+++ lib24.02.23.sql
@@ -2,8 +2,11 @@ CREATE TABLE USERS(
 	USER_CODE INT AUTO_INCREMENT PRIMARY KEY
 	, USER_ID VARCHAR(20) NOT NULL
 	, USER_PW VARCHAR(50) NOT NULL 
+	, USER_NAME VARCHAR(20) NOT NULL
 	, USER_TEL VARCHAR(20) NOT NULL
-	, USER_ADDR VARCHAR(100) NOT NULL
+	, POST_CODE INT NOT NULL
+	, USER_ADDR VARCHAR(50) NOT NULL
+	, ADDR_DETAIL VARCHAR(50) NOT NULL
 	, GENDER VARCHAR(2) NOT NULL
 	, EMAIL VARCHAR(50) NOT NULL
 	, IS_ADMIN VARCHAR(2) DEFAULT 'N'); -- N,Y
@@ -12,7 +15,7 @@ CREATE TABLE BOOK(
    BOOK_CODE VARCHAR(15) PRIMARY KEY,
    BOOK_TITLE VARCHAR(100) NOT NULL,
    BOOK_WRITER VARCHAR(100) NOT NULL,
-   BOOK_PUB VARCHAR(10) NOT NULL,
+   BOOK_PUB VARCHAR(30) NOT NULL,
    BOOK_YEAR VARCHAR(20) NOT NULL);
    
 LOAD DATA LOCAL INFILE 'D:/01-STUDY/insertData_2.csv'
@@ -21,8 +24,94 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 ;
 
+CREATE TABLE HEADER_MENU(
+	MENU_NUM INT AUTO_INCREMENT PRIMARY KEY ,
+	MENU_NAME VARCHAR(20) NOT NULL ,
+	MENU_PAGE VARCHAR(50) NOT NULL ,
+	MENU_INDEX INT NOT NULL);
+	
+-- DROP TABLE header_menu;
+
+CREATE TABLE SIDE_MENU(
+	SIDE_MENU_NUM INT AUTO_INCREMENT PRIMARY KEY,
+	SIDE_MENU_NAME VARCHAR(20) NOT NULL ,
+	SIDE_MENU_PAGE VARCHAR(50) NOT NULL ,
+	SIDE_MENU_INDEX INT NOT NULL ,
+	MENU_NUM INT REFERENCES HEADER_MENU(menu_num));
+
+-- DROP TABLE side_menu;
+
+-- 헤더 파일 데이터
+INSERT INTO HEADER_MENU(
+	MENU_NAME , MENU_PAGE , MENU_INDEX)
+	VALUES
+	('대출 반납', 'borrowReturn', 1),
+	('이용자', 'user', 2) ,
+	('구입', 'buy', 3) ,
+	('등록 열람', 'regAndView', 4) ,
+	('통계', 'statistics', 5);
+
+SELECT * FROM header_menu;
+SELECT * FROM side_menu;
+
+-- 사이드 메뉴 데이터
+INSERT INTO SIDE_MENU(
+	SIDE_MENU_NAME ,
+	SIDE_MENU_PAGE ,
+	SIDE_MENU_INDEX ,
+	MENU_NUM)
+	VALUES 
+	-- 대출 반납
+	('대출 반납', 'borrowReturn', 1 , 1),
+	('일관 반납', 'consistentReturn', 2 , 1),
+	('대출 반납 관리', 'borrowReturnManagement', 3 , 1),
+	('예약정보 관리', 'reservationInfo', 4 , 1),
+	('출력이력 관리', 'outputHistory', 5 , 1) ,
+	-- 이용자
+	('이용자 관리', 'user' , 1 , 2) ,
+	('이용자 승인', 'userApproval', 2 , 2) ,
+	('연체자 관리', 'delinquent', 3 , 2) ,
+	-- 구입
+	('희망 자료' , 'wishBook' , 1 , 3) ,
+	('삭제 자료' , 'deleteBook', 2 , 3) ,
+	('구입 자료' , 'buyBook', 3 , 3) ,
+	('기증 자료' , 'donatedBook', 4 , 3) ,
+	-- 등록 열람
+	('작업 자료 관리', 'workingBook', 1, 4),
+	('소장 자료 관리', 'collectionBook', 2, 4),
+	('마크 반입', 'markImport', 3, 4),
+	-- 통계
+	('통계' , 'statistics', 1, 5);
+	
+UPDATE side_menu
+SET side_menu_page = 'workingBook'
+WHERE side_menu_name = '작업 자료 관리';
+
+-- 메뉴들 찾기 위한 쿼리문
+SELECT 
+	header_menu.MENU_NUM ,
+	MENU_NAME ,
+	MENU_PAGE ,
+	MENU_INDEX ,
+	SIDE_MENU_NAME ,
+	SIDE_MENU_PAGE ,
+	SIDE_MENU_INDEX ,
+	SIDE_MENU.SIDE_MENU_NUM
+FROM
+	header_menu INNER JOIN side_menu
+	ON header_menu.MENU_NUM = side_menu.MENU_NUM
+WHERE
+	header_menu.MENU_NUM = 1;
+	
+-- culturalAndEducation
+-- findBook
+-- home
+-- libraryIntroduction
+-- member
+-- participationForum
 
 
+DROP TABLE USERS;
 
 DROP TABLE book;
 
